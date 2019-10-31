b0ba460daec0daa6c283e453940a01abadcf1614
meite-root <52190203+meite-root@users.noreply.github.com>
2019-10-31 19:01:56 +0400

    Update .env.py

--- .env.py
+++ .env.py
@@ -1,3 +1,3 @@
 SECRET_KEY=rqr_cjv4igscyu8&&(0ce(=sy=f2)p=f_wn&@0xsp7m$@!kp=d
-DEBUG=True
-ALLOWED_HOSTS=.localhost,127.0.0.1
+ALLOWED_HOSTS=.ivoredu.com
+DATABASE_URL=postgres://u_afroed2:databaseforafroed2020@localhost:5432/afroed_database
